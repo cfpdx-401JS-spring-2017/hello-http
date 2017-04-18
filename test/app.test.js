da7a084371f761dcd3d6bcc58abeeed1f3fd0f8f
nicky evers <livelyevers@gmail.com>
2017-04-18 16:43:52 -0700

    got logs post working with test working

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,10 @@
-const chai = require('chai-http');
+const chai = require('chai');
 const assert = chai.assert;
 const chaiHttp = require('chai-http');
+const rimraf = require('rimraf');
+const cowsay = require('cowsay');
+const fs = require('fs');
+
 const app = require('../lib/app');
 
 chai.use(chaiHttp);
@@ -9,12 +13,88 @@ describe('app', () => {
 
   const request = chai.request(app);
 
-  it('says hello stranger', done => {
-    request.get('/greeting')
+  describe('/greeting', () => {
+
+    it('says hello stranger as default', done => {
+      request.get('/greeting')
         .end((err, res) => {
-          assert.equal(res.text, 'hello stranger');
+          if (err) return done(err);
+          assert.equal(res.text, '<pre>' + cowsay.say({
+            text: 'hello stranger',
+            e: "oO",
+            T: "U "
+          }) + '</pre>');
           done();
         });
+    });
+
+    it('says salutation <name> when correct query is input', done => {
+      request.get('/greeting?salutation=nicky')
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.text, '<pre>' + cowsay.say({
+            text: 'salutations nicky',
+            e: "oO",
+            T: "U "
+          }) + '</pre>');
+          done();
+        });
+    });
+  }),
+
+  describe('fact', () => {
+
+    it('serves fact.html when that page is navigated to', done => {
+      request.get('/fact')
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res)
+        });
+    });
+  }),
+
+  describe('notfound', () => {
+
+    it('returns status code 404 when resource is not found', done => {
+      request.get('/ashde')
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.statusCode, 404);
+          done();
+        });
+    });
+  }),
+
+  describe('POST /logs', () => {
+
+    before(done => {
+      rimraf('./logs', err => {
+        if (err) return done(err);
+        done();
+      })
+    });
+
+    it('creates a logs directory if none exists and returns 201', done => {
+      const postData = { text: 'blah' }
+      request
+        .post('/logs')
+        .send(postData)
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.statusCode, 201);
+
+          fs.readdir('./logs', (err, files) => {
+            if (err) return done(err);
+            assert.equal(files.length, 1);
+
+            fs.readFile(`./logs/${files[0]}`, (err, data) => {
+              if (err) return done(err);
+              assert.deepEqual(JSON.parse(data), postData)
+              done();
+            });
+          });
+        });
+    });
   });
 });
-// I can't get the tests to run successfuly, i keep getting errors telling me that chai.use or chai.request are not function, but I have everything setup correctly as far as I can tell.
+
