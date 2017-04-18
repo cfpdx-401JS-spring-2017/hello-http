d066fa2827574be58a0c2c7aa96f511a927e88e5
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-18 16:50:14 -0700

    Lab 2 completed

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const server = http.createServer(app);
 const port = 3000;
 
 server.listen(port, () => {
-  console.log('Server running on PORT', server.address().port);
+  console.log('Server running on PORT', server.address().port); //eslint-disable-line
 });
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
-const assert = chai.assert;
 const chaiHttp = require('chai-http');
+const assert = chai.assert;
+
+const rimraf = require('rimraf');
+const fs = require('fs');
 const app = require('../lib/app');
 
 chai.use(chaiHttp);
@@ -9,54 +12,94 @@ describe('app', () => {
 
   const request = chai.request(app);
 
-  it('says hello stranger', done => {
-    request.get('/hello')
-      .end((err, res) => {
-        assert.equal(res.text, 'Hello stranger');
-        done();
-      });
+  describe('/greeting', () => {
+
+    it('says hello stranger', done => {
+      request.get('/hello')
+        .end((err, res) => {
+          assert.equal(res.text, 'Hello stranger');
+          done();
+        });
+    });
+
+    it('greets a person with a name', done => {
+      const name = 'Dave';
+      request.get(`/hello?name=${name}`)
+        .end((err, res) => {
+          assert.equal(res.text, 'Hello Dave');
+          done();
+        });
+    });
+
+    it('greets a person with a custom salutation', done => {
+      const salutation = 'Hey hey hey';
+      const name = 'Dave';
+      request.get(`/hello?name=${name}&salutation=${salutation}`)
+        .end((err, res) => {
+          assert.equal(res.text, 'Hey hey hey Dave');
+          done();
+        });
+    });
+
+    it('responds with fun fact', done => {
+      request.get('/fact')
+        .end((err, res) => {
+          assert.equal(res.text, 'Did you know that a pomeranian watched Michealangelo paint the Sistine Chapel? It\'s true.');
+          done();
+        });
+    });
+
+    it('serves contents of /index', done => {
+      request.get('/')
+        .end((err, res) => {
+          assert.match(res.text, /This website is full of fluffy dogs./);
+          done();
+        });
+    });
+
+    it('returns a 404', done => {
+      request.get('/doesnotexist')
+        .end((err, res) => {
+          assert.equal(res.res.statusMessage, 'CANNOT GET /doesnotexist');
+          done();
+        });
+    });
+
   });
 
-  it('greets a person with a name', done => {
-    const name = 'Dave';
-    request.get(`/hello?name=${name}`)
-      .end((err, res) => {
-        assert.equal(res.text, 'Hello Dave');
+  describe('/post', () => {
+
+    before(done => {
+      rimraf('./logs', err => {
+        if (err) return done(err);
         done();
       });
-  });
+    });
 
-  it('greets a person with a custom salutation', done => {
-    const salutation = 'Hey hey hey';
-    const name = 'Dave';
-    request.get(`/hello?name=${name}&salutation=${salutation}`)
-      .end((err, res) => {
-        assert.equal(res.text, 'Hey hey hey Dave');
-        done();
-      });
-  });
-
-  it('responds with fun fact', done => {
-    request.get('/fact')
-      .end((err, res) => {
-        assert.equal(res.text, 'Did you know that a pomeranian watched Michealangelo paint the Sistine Chapel? It\'s true.');
-        done();
-      });
-  });
-
-  it('serves contents of /index', done => {
-    request.get('/')
-      .end((err, res) => {
-        assert.match(res.text, /This website is full of fluffy dogs./);
-        done();
-      });
-  });
-
-  it('returns a 404', done => {
-    request.get('/doesnotexist')
-    .end((err, res) => {
-      assert.equal(res.res.statusMessage, 'CANNOT GET /doesnotexist'); 
-      done();
+    it.only('POST /logs, creates directory', done => {
+      const postData = {
+        text: 'blah',
+        greeting: 'hey hey hey' };
+      
+      request
+        .post('/logs')
+        .send(postData)
+        .end((err, res) => {
+          if (err) return done(err);
+          assert.equal(res.statusCode, 201);
+          
+          fs.readdir('./logs', (err, files) => {
+            if (err) return done(err);
+            assert.equal(files.length, 1);
+          
+            fs.readFile(`./logs/${files[0]}`, (err, data) => {
+              if (err) return done(err);
+              assert.deepEqual(JSON.parse(data), postData);
+              done();
+            });
+          });
+        });
     });
   });
+
 });
